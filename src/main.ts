2c2357eb44f228bb25548c1d65ca6543bb4d8746
Pedro Henrique Reis <contatodopedroreis@gmail.com>
2024-10-24 12:02:49 -0300

    fix: fixing docker configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, { cors: true, });
+  await app.listen(process.env.PORT, () =>
+    Logger.log(`Listening in port ${process.env.PORT}`, 'NestApplication'),
+  );
 }
 bootstrap();
