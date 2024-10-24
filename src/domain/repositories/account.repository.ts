778b0441447b42c70e630368bcdc972c33d4db5f
Pedro Henrique Reis <contatodopedroreis@gmail.com>
2024-10-24 17:03:50 -0300

    refactor: adjusting return at findUniqueByName and running lint

--- src/domain/repositories/account.repository.ts
+++ src/domain/repositories/account.repository.ts
@@ -4,7 +4,7 @@ import { PrismaService } from 'src/infrastructure/database/connection/prisma.ser
 
 @Injectable()
 export class AccountRepository {
-  constructor(private readonly prisma: PrismaService) { }
+  constructor(private readonly prisma: PrismaService) {}
 
   private get account() {
     return this.prisma.account;
@@ -27,11 +27,11 @@ export class AccountRepository {
   }
 
   async findUniqueByName(firstName: string, lastName: string): Promise<any> {
-    const account = await this.account.findFirst({
+    return await this.account.findFirst({
       where: {
         AND: {
           firstName: { equals: firstName },
-          lastName: { equals: lastName }
+          lastName: { equals: lastName },
         },
       },
     });
@@ -40,11 +40,11 @@ export class AccountRepository {
   async addValue(cuid: string, balance: number): Promise<Account> {
     return await this.account.update({
       where: {
-        cuid: cuid
+        cuid: cuid,
       },
       data: {
-        balance: balance
-      }
-    })
+        balance: balance,
+      },
+    });
   }
 }
