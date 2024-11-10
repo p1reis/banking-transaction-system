2853488154066086770b4a049353d89359d39363
Pedro Henrique Reis <contatodopedroreis@gmail.com>
2024-11-10 13:25:33 -0300

    build: update configuration file

--- src/shared/config/configuration.ts
+++ src/shared/config/configuration.ts
@@ -8,7 +8,13 @@ export default () => ({
     password: process.env.POSTGRES_PASSWORD,
     port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
     database: process.env.POSTGRES_DB,
+    timezone: process.env.POSTGRES_TZ,
     schema: process.env.POSTGRES_SCHEMA,
     url: process.env.DATABASE_URL,
   },
+
+  redis: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT
+  }
 });
