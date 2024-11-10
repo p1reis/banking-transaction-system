82c6ebe55345a41f10ec3e4570527fc4a92f3e05
Pedro Henrique Reis <contatodopedroreis@gmail.com>
2024-11-10 13:20:13 -0300

    feat: add luhn validation to number accounts

--- src/shared/utils/generate-account-number.ts
+++ src/shared/utils/generate-account-number.ts
@@ -6,8 +6,39 @@ export class GenerateAccountNumberUtils {
   private chance = new Chance();
 
   generateAccountNumber(): string {
-    const parte1 = this.chance.integer({ min: 1000, max: 9999 }).toString();
-    const parte2 = this.chance.integer({ min: 10, max: 99 }).toString();
-    return `${parte1}-${parte2}`;
+    let accountNumber: string;
+    let isValid = false;
+
+    while (!isValid) {
+      const parte1 = this.chance.integer({ min: 10000, max: 99999 }).toString();
+      const parte2 = this.chance.integer({ min: 10, max: 99 }).toString();
+
+      accountNumber = `${parte1}-${parte2}`;
+
+      isValid = this.luhnCheck(accountNumber.replace('-', ''));
+    }
+
+    return accountNumber;
+  }
+
+  private luhnCheck(number: string): boolean {
+    let sum = 0;
+    let shouldDouble = false;
+
+    for (let i = number.length - 1; i >= 0; i--) {
+      let digit = parseInt(number.charAt(i), 10);
+
+      if (shouldDouble) {
+        digit *= 2;
+        if (digit > 9) {
+          digit -= 9;
+        }
+      }
+
+      sum += digit;
+      shouldDouble = !shouldDouble;
+    }
+
+    return sum % 10 === 0;
   }
 }
